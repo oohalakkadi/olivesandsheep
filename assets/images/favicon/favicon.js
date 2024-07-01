7b7e19302ddc2175ff88ee0cf4904afce1a127a5
oohalakkadi <ooha.reddy@duke.edu>
2024-06-30 21:57:28 -0700

    More org

---
+++ assets/images/favicon/favicon.js
@@ -0,0 +1,19 @@
+function iconChange() {
+    const frames = [
+        "assets/images/favicon/favicon frames/favicon-1.png",
+        "assets/images/favicon/favicon frames/favicon-2.png",
+        "assets/images/favicon/favicon frames/favicon-3.png",
+        "assets/images/favicon/favicon frames/favicon-4.png",
+        "assets/images/favicon/favicon frames/favicon-5.png",
+        "assets/images/favicon/favicon frames/favicon-6.png"
+    ];
+    
+    let currentFrame = 0;
+
+    setInterval(function() {
+        document.getElementById("icon").href = frames[currentFrame];
+        currentFrame = (currentFrame + 1) % frames.length;
+    }, 250);
+}
+
+window.onload = iconChange;
