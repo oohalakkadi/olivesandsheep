197d96b9fc954573df6da6bd9f692c54b4c8ee69
oohalakkadi <ooha.reddy@duke.edu>
2024-07-02 04:53:27 -0700

    first spiderfy attempt

--- olives and sheep/scripts/olives.js
+++ olives and sheep/scripts/olives.js
@@ -8,241 +8,268 @@ const filters = {
   'goods': ['==', 'Format', 'Goods & Services']
 };
 
-let originalData; // Variable to store original GeoJSON data
+const SPIDERFY_FROM_ZOOM = 14;
+let originalData;
 
-$(document).ready(function () {
-  $.ajax({
-    type: "GET",
-    url: 'https://docs.google.com/spreadsheets/d/19FiVC6W4ncKi86eWhYK5Udh6p0ud-aaXuyJW8gpngc0/gviz/tq?tqx=out:csv&sheet=Sheet1',
-    dataType: "text",
-    success: function (csvData) {
-      makeGeoJSON(csvData);
-    }
-  });
+$.ajax({
+  type: "GET",
+  url: 'https://docs.google.com/spreadsheets/d/19FiVC6W4ncKi86eWhYK5Udh6p0ud-aaXuyJW8gpngc0/gviz/tq?tqx=out:csv&sheet=Sheet1',
+  dataType: "text",
+  success: function (csvData) {
+    makeGeoJSON(csvData);
+  }
+});
 
-  function makeGeoJSON(csvData) {
-    csv2geojson.csv2geojson(csvData, {
-      latfield: 'Latitude',
-      lonfield: 'Longitude',
-      delimiter: ','
-    }, function (err, data) {
-      originalData = data; // Store original data
-      map.on('load', function () {
-        addLayers(data);
-        attachEventHandlers(); // Attach handlers after layers are added
+function makeGeoJSON(csvData) {
+  csv2geojson.csv2geojson(csvData, {
+    latfield: 'Latitude',
+    lonfield: 'Longitude',
+    delimiter: ','
+  }, function (err, data) {
+    originalData = data;
+    map.on('load', function () {
+      addLayers(data);
+      attachEventHandlers();
+    });
+
+    map.on('styledata', function () {
+      addLayers(data);
+      attachEventHandlers();
+    });
+
+    function addLayers(data) {
+      map.addSource('data', {
+        type: 'geojson',
+        data: data,
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50
       });
 
-      map.on('styledata', function () {
-        addLayers(data);
-        attachEventHandlers(); // Reattach handlers if style is reloaded
+      map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'data',
+        filter: ['has', 'point_count'],
+        paint: {
+          'circle-color': ['step', ['get', 'point_count'], '#7851B7', 5, '#633E9E', 10, '#412272'],
+          'circle-radius': ['step', ['get', 'point_count'], 20, 5, 30, 10, 40]
+        }
       });
 
-      function addLayers(data) {
-        // Add GeoJSON source with clustering
-        map.addSource('data', {
-          type: 'geojson',
-          data: data,
-          cluster: true,
-          clusterMaxZoom: 14,
-          clusterRadius: 50
-        });
+      map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'data',
+        filter: ['has', 'point_count'],
+        layout: {
+          'text-field': '{point_count_abbreviated}',
+          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+          'text-size': 12
+        },
+        paint: {
+          'text-color': 'white'
+        }
+      });
 
-        // Add clustered layer
+      symbolLayers.forEach(layerId => {
         map.addLayer({
-          id: 'clusters',
-          type: 'circle',
-          source: 'data',
-          filter: ['has', 'point_count'],
-          paint: {
-            'circle-color': [
-              'step',
-              ['get', 'point_count'],
-              '#7851B7',
-              5,
-              '#633E9E',
-              10,
-              '#412272'
-            ],
-            'circle-radius': [
-              'step',
-              ['get', 'point_count'],
-              20,
-              5,
-              30,
-              10,
-              40
-            ]
-          }
-        });
-
-        // Add cluster count layer
-        map.addLayer({
-          id: 'cluster-count',
+          id: layerId,
           type: 'symbol',
           source: 'data',
-          filter: ['has', 'point_count'],
+          filter: filters[layerId],
           layout: {
-            'text-field': '{point_count_abbreviated}',
-            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-            'text-size': 12
-          },
-          paint: {
-            'text-color': 'white'
+            'icon-image': layerId === 'articles' ? 'articles' : layerId === 'reports' ? 'reports' : layerId === 'photos' ? 'photos' : layerId === 'videos' ? 'videos' : layerId === 'social-media' ? 'social-photo' : layerId === 'goods' ? 'goods' : '',
+            'icon-size': 1.2,
+            'icon-allow-overlap': true,
+            'icon-ignore-placement': true
           }
         });
+      });
 
-        // Add other layers with filters
-        symbolLayers.forEach(layerId => {
-          map.addLayer({
-            id: layerId,
-            type: 'symbol',
-            source: 'data',
-            filter: filters[layerId],
-            layout: {
-              'icon-image': layerId === 'articles' ? 'articles' :
-                layerId === 'reports' ? 'reports' :
-                  layerId === 'photos' ? 'photos' :
-                    layerId === 'videos' ? 'videos' :
-                    layerId === 'social-media' ? 'social-photo' :
-                      layerId === 'goods' ? 'goods' : '',
-              'icon-size': 1.2,
-              'icon-allow-overlap': true,
-              'icon-ignore-placement': true
-            }
-          });
+
+      const spiderifier = new MapboxglSpiderifier(map, {
+        animate: true,
+        animationSpeed: 200,
+        customPin: true,
+        onClick: function (e, spiderLeg) {
+          const feature = spiderLeg.feature;
+          const popupContent = `
+                  <h3>${feature.properties.Name}</h3>
+                  <h4><em>${feature.properties.Date}</em></h4>
+                  <h4><b>${feature.properties.Address}</b></h4>
+                  <h4>${feature.properties.About}</h4>
+                  <h4><a href='${feature.properties.Link}'>${feature.properties.Hyperlink}</a></h4>
+                `;
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+          new mapboxgl.Popup()
+            .setLngLat(spiderLeg.mapboxMarker.getLngLat())
+            .setHTML(popupContent)
+            .addTo(map);
+        },
+        initializeLeg: function (spiderLeg) {
+          const pinElem = spiderLeg.elements.pin;
+          const feature = spiderLeg.feature;
+
+          let iconImage;
+          switch (feature.properties.Format) {
+            case 'Articles':
+              iconImage = 'articles';
+              break;
+            case 'Reports':
+              iconImage = 'reports';
+              break;
+            case 'Photos':
+              iconImage = 'photos';
+              break;
+            case 'Videos':
+              iconImage = 'videos';
+              break;
+            case 'Social Media':
+              iconImage = 'social-photo';
+              break;
+            case 'Goods & Services':
+              iconImage = 'goods';
+              break;
+          }
+
+          pinElem.style.backgroundImage = `url(${map.style.getImage(iconImage).url})`;
+          pinElem.style.backgroundSize = 'contain';
+          pinElem.style.width = '40px';
+          pinElem.style.height = '40px';
+        }
+      });
+
+      map.on('click', 'clusters', function (e) {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ['clusters']
         });
-
-        function addLayerFunctionality(layerId) {
-          map.on('click', 'clusters', (e) => {
-            const features = map.queryRenderedFeatures(e.point, {
-              layers: ['clusters']
+        const clusterId = features[0].properties.cluster_id;
+        map.getSource('data').getClusterExpansionZoom(clusterId, function (err, zoom) {
+          if (err) return;
+          if (zoom < SPIDERFY_FROM_ZOOM) {
+            map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom: zoom
             });
-            const clusterId = features[0].properties.cluster_id;
-            map.getSource('data').getClusterExpansionZoom(
+          } else {
+            map.getSource('data').getClusterLeaves(
               clusterId,
-              (err, zoom) => {
-                if (err) return;
-
-                map.easeTo({
-                  center: features[0].geometry.coordinates,
-                  zoom: zoom
-                });
+              100,
+              0,
+              function (err, leafFeatures) {
+                if (err) return console.error(err);
+                const markers = leafFeatures.map(leafFeature => ({
+                  ...leafFeature,
+                  properties: {
+                    ...leafFeature.properties,
+                    iconType: 'marker',
+                    iconColor: 'red'
+                  }
+                }));
+                spiderifier.spiderfy(features[0].geometry.coordinates, markers);
               }
             );
-          });
-
-          map.on('click', layerId, function (e) {
-            var coordinates = e.features[0].geometry.coordinates.slice();
-            var description = `
-                <h3>${e.features[0].properties.Name}</h3>
-                <h4><em>${e.features[0].properties.Date}</em></h4>
-                <h4><b>${e.features[0].properties.Address}</b></h4>
-                <h4>${e.features[0].properties.About}</h4>
-                <h4><a href='${e.features[0].properties.Link}'>${e.features[0].properties.Hyperlink}</a></h4>
-              `;
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-              coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            }
-            new mapboxgl.Popup()
-              .setLngLat(coordinates)
-              .setHTML(description)
-              .addTo(map);
-          });
-
-          map.on('mouseenter', layerId, function () {
-            map.getCanvas().style.cursor = 'pointer';
-          });
-
-          map.on('mouseleave', layerId, function () {
-            map.getCanvas().style.cursor = '';
-          });
-
-          var bbox = turf.bbox(data);
-          map.fitBounds(bbox, { padding: 50 });
-        }
-
-        symbolLayers.forEach(layerId => {
-          addLayerFunctionality(layerId);
-        });
-      }
-    });
-  }
-
-  function updateClusterData() {
-    let filteredFeatures = originalData.features.filter(feature => {
-      let format = feature.properties.Format;
-      return !symbolLayers.some(layerId => {
-        const visibility = map.getLayoutProperty(layerId, 'visibility');
-        return visibility === 'none' && filters[layerId][2] === format;
-      });
-    });
-
-    let updatedData = {
-      ...originalData,
-      features: filteredFeatures
-    };
-
-    map.getSource('data').setData(updatedData);
-  }
-
-  function handleCheckboxChange() {
-    const layerId = this.id;
-    const layer = map.getLayer(layerId);
-
-    if (layer) { // Check if layer exists
-      try {
-        if (this.checked) {
-          map.setLayoutProperty(layerId, 'visibility', 'visible');
-        } else {
-          map.setLayoutProperty(layerId, 'visibility', 'none');
-        }
-      } catch (e) {
-        console.error(`Error setting visibility for layer ${layerId}:`, e);
-      }
-
-      updateClusterData();
-    }
-  }
-
-  function attachEventHandlers() {
-    // Ensure elements exist before attaching handlers
-    if ($('#toggle-olives-sheep').length && $('.olives-sheep-sub').length) {
-      $('#toggle-olives-sheep').off('change').on('change', function () {
-        const checked = this.checked;
-
-        $('.olives-sheep-sub').each(function () {
-          this.checked = checked;
-
-          const layer = map.getLayer(this.id);
-          if (layer) { // Check if layer exists
-            try {
-              map.setLayoutProperty(this.id, 'visibility', checked ? 'visible' : 'none');
-            } catch (e) {
-              console.error(`Error setting visibility for layer ${this.id}:`, e);
-            }
           }
         });
-
-        updateClusterData();
       });
 
-      $('.olives-sheep-sub').off('change').on('change', function () {
-        const anyChecked = $('.olives-sheep-sub:checked').length > 0;
-        $('#toggle-olives-sheep').prop('checked', anyChecked);
-        handleCheckboxChange.call(this);
+      symbolLayers.forEach(layerId => {
+        map.on('click', layerId, function (e) {
+          const coordinates = e.features[0].geometry.coordinates.slice();
+          const description = `
+            <h3>${e.features[0].properties.Name}</h3>
+            <h4><em>${e.features[0].properties.Date}</em></h4>
+            <h4><b>${e.features[0].properties.Address}</b></h4>
+            <h4>${e.features[0].properties.About}</h4>
+            <h4><a href='${e.features[0].properties.Link}'>${e.features[0].properties.Hyperlink}</a></h4>
+          `;
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+          new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+        });
+
+        map.on('mouseenter', layerId, function () {
+          map.getCanvas().style.cursor = 'pointer';
+        });
+
+        map.on('mouseleave', layerId, function () {
+          map.getCanvas().style.cursor = '';
+        });
       });
-    }
-  }
-
-  map.on('idle', () => {
-    const toggleableLayerIds = ['articles', 'reports', 'photos', 'videos', 'social-media', 'goods'];
-
-    for (const id of toggleableLayerIds) {
-      const checkbox = document.getElementById(id);
-      if (checkbox) {
-        checkbox.removeEventListener('change', handleCheckboxChange); // Remove previous handlers
-        checkbox.addEventListener('change', handleCheckboxChange);
-      }
     }
   });
+}
+
+function updateClusterData() {
+  let filteredFeatures = originalData.features.filter(feature => {
+    let format = feature.properties.Format;
+    return !symbolLayers.some(layerId => {
+      const visibility = map.getLayoutProperty(layerId, 'visibility');
+      return visibility === 'none' && filters[layerId][2] === format;
+    });
+  });
+
+  let updatedData = { ...originalData, features: filteredFeatures };
+  map.getSource('data').setData(updatedData);
+}
+
+function handleCheckboxChange() {
+  const layerId = this.id;
+  const layer = map.getLayer(layerId);
+  if (layer) {
+    try {
+      if (this.checked) {
+        map.setLayoutProperty(layerId, 'visibility', 'visible');
+      } else {
+        map.setLayoutProperty(layerId, 'visibility', 'none');
+      }
+    } catch (e) {
+      console.error(`Error setting visibility for layer ${layerId}:`, e);
+    }
+    updateClusterData();
+  }
+}
+
+function attachEventHandlers() {
+  if ($('#toggle-olives-sheep').length && $('.olives-sheep-sub').length) {
+    $('#toggle-olives-sheep').off('change').on('change', function () {
+      const checked = this.checked;
+      $('.olives-sheep-sub').each(function () {
+        this.checked = checked;
+        const layer = map.getLayer(this.id);
+        if (layer) {
+          try {
+            map.setLayoutProperty(this.id, 'visibility', checked ? 'visible' : 'none');
+          } catch (e) {
+            console.error(`Error setting visibility for layer ${this.id}:`, e);
+          }
+        }
+      });
+      updateClusterData();
+    });
+
+    $('.olives-sheep-sub').off('change').on('change', function () {
+      const anyChecked = $('.olives-sheep-sub:checked').length > 0;
+      $('#toggle-olives-sheep').prop('checked', anyChecked);
+      handleCheckboxChange.call(this);
+    });
+  }
+}
+
+map.on('idle', () => {
+  const toggleableLayerIds = ['articles', 'reports', 'photos', 'videos', 'social-media', 'goods'];
+  for (const id of toggleableLayerIds) {
+    const checkbox = document.getElementById(id);
+    if (checkbox) {
+      checkbox.removeEventListener('change', handleCheckboxChange);
+      checkbox.addEventListener('change', handleCheckboxChange);
+    }
+  }
 });
